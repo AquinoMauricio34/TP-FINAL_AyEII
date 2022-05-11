676a5c5951830ecd876ec08163c7207312c5aef7
Melani Schneider <melanischneider15@gmail.com>
2022-05-11 17:22:23 -0300

    recorrer_turnos

---
+++ funciones/turno_cliente/recorrer_turnos.c
@@ -0,0 +1,19 @@
+#ifndef recorrer_turnos_C
+#define recorrer_turnos_C
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"../../estructuras.h"
+#include"../../prototipos.h"
+void recorrer_turnos(int cod_act,tipo_turno *ini_turno){
+	int buscar=0;
+	while(ini_turno != NULL){
+		if(ini_turno->cod_act == cod_act){
+			printf("%d,%f,%d",ini_turno->cod_turno,ini_turno->precio,//chequear VECTOR ini_turno->dias);
+		}
+		ini_actividad = ini_actividad->sgte;
+	}
+	
+}
+#endif
