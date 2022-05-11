76d8587c2762d2b6177f72809a6c64c65cd32144
Melani Schneider <melanischneider15@gmail.com>
2022-05-11 17:11:00 -0300

    hola melani 11/05 17

--- prototipos.h
+++ prototipos.h
@@ -11,7 +11,7 @@ int buscar_actividades(int codigo,actividad *ini_actividad);
 void buscar_borrar_profesor(long int dato,profesor **bor,profesor **ant);
 void insertar_profesor(profesor **nv, profesor **ini_profesor);
 void listar_codigo_actividades(actividad *ini);
-void modificar_profesor(long int dato,int op,profesor **ini_profesor);
+void modificar_profesor(long int dato,int op,profesor **ini_profesor,actividad *ini_actividad);
 void vacio_reserva(reserva *S);
 // clientes
 void ABM_clientes(cliente **ini_cliente,actividad **ini_actividad);
