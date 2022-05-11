005f023a1fcaf53a0d0e88cc0c1c3e733c9436ed
Daiana sienra <dai.sienra@gmail.com>
2022-05-10 18:13:19 -1200

    daiana10/05

--- funciones/clientes/ABM_cliente.c
+++ funciones/clientes/ABM_cliente.c
@@ -8,6 +8,7 @@
 #include"../../prototipos.h"
 void ABM_clientes(cliente **ini_cliente,actividad **ini_actividad){
 	cliente *nv=NULL;
+	actividad *p=NULL;
 	int op,buscar=0,dni_cliente,buscar_dni=0,op_mod;
 	long int buscar_borrar,modificar;
 	do{
@@ -23,9 +24,9 @@ void ABM_clientes(cliente **ini_cliente,actividad **ini_actividad){
 				do{//mientras se encuentre el dni ingresado o el dni es igual a 0, el bucle se repetira
 					listar_codigo_actividades(*ini_actividad);
 					printf("ingrese el codigo de la actividad que desee\n");
-                    scanf("%d",&nv->cod_act);
-					buscar = buscar_actividades(nv->cod_act,*ini_actividad);
-				}while(buscar!=1 && nv->cod_act !=0);
+                    scanf("%d",&p->cod_act);
+					buscar = buscar_actividades(p->cod_act,*ini_actividad);
+				}while(buscar!=1 && p->cod_act !=0);
 
 				if(buscar == 1){
 					do{
@@ -35,7 +36,7 @@ void ABM_clientes(cliente **ini_cliente,actividad **ini_actividad){
 					}while(buscar_dni != 1 && nv->dni !=0);
 					if(nv->dni != 0){
 						printf("ingrese el nombre del cliente\n");
-						scanf("%s",nv->nombre);
+						gets(nv->nombre);
 						printf("ingrese el telefono del cliente\n");
 						scanf("%ld",&nv->telefono);
 						insertar_cliente(&nv,&*ini_cliente);
@@ -52,8 +53,10 @@ void ABM_clientes(cliente **ini_cliente,actividad **ini_actividad){
 				}while(buscar_dni != 1 && buscar_borrar !=0);
 
 				if(buscar_borrar != 0){//si ebcontro el dni
-					printf("Esta seguro/a de que quiere eliminar al cliente (1. SI | 0. NO): ");scanf("%d",&op);
+					printf("Esta seguro/a de que quiere eliminar al cliente (1. SI | 0. NO): ");
+					scanf("%d",&op);
 					if(op == 1){
+					//	borrar_Tcliente(&*ini_cliente);
 						borrar_nodo_cliente(buscar_borrar,&*ini_cliente);
 					}
 				}
