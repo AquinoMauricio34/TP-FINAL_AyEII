d7ff24ec8624537c7a03ddc4106b9ce8c9d44a46
AquinoMauricio34 <92173824+AquinoMauricio34@users.noreply.github.com>
2022-05-11 00:14:56 -0300

    Update borrar_Tcliente.c

---
+++ funciones/clientes/borrar_Tcliente.c
@@ -0,0 +1,15 @@
+
+
+
+
+/*
+tene en cuenta que al momento de borrar los nodos, puede que uno de los nodos a borrar
+sea el que es apuntado por el inicio de la lista, cuidado de no borrarlo
+y perder el inicio de la lista
+*/
+
+
+
+
+
+
