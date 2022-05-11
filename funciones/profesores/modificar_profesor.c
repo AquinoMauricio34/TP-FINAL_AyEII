3e62075d4e396ed30d9d5bb7d2cf2816ff18a1b8
AquinoMauricio34 <92173824+AquinoMauricio34@users.noreply.github.com>
2022-05-11 16:55:02 -0300

    Update modificar_profesor.c

--- funciones/profesores/modificar_profesor.c
+++ funciones/profesores/modificar_profesor.c
@@ -6,7 +6,8 @@
 #include<string.h>
 #include"../../estructuras.h"
 #include"../../prototipos.h"
-void modificar_profesor(long int dato,int op,profesor **ini_profesor){
+void modificar_profesor(long int dato,int op,profesor **ini_profesor, actividad *ini_actividad){
+	int buscar,codigo;
 	while(*ini_profesor != NULL){
 		
 		if((*ini_profesor)->dni == dato){
@@ -16,6 +17,19 @@ void modificar_profesor(long int dato,int op,profesor **ini_profesor){
 				gets((*ini_profesor)->nombre);
 			}else if(op==2){
 				scanf("%ld",&(*ini_profesor)->telefono);
+			}else if(op==3){
+				do{
+					system("cls");
+					printf("\nActividades Disponibles\n");
+					listar_actividades(ini_actividad);
+					printf("Ingresar codigo de la actividad deseada: \n");
+					scanf("%d",&codigo);
+					buscar = buscar_actividades(codigo,ini_actividad);
+				}while(buscar != 1 || codigo != 0);
+				
+				if(buscar == 1){
+					(*ini_profesor)->cod_act = codigo;
+				}
 			}
 			*ini_profesor = NULL;
 		}
